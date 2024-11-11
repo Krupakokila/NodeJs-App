2594837c224e7ddafa9eccf66a15e4d4d3d5a5fc
Krupakokila <krupa.kokila@thebluspire.com>
2024-11-11 12:36:06 +0530

    firts

---
+++ tests/app.test.js
@@ -0,0 +1,8 @@
+describe("Sample test case", () => {
+  test("First test case", () => {
+    expect(1 + 1).toBe(2);
+  });
+  test("second test case", () => {
+    expect(1 + 2).toBe(3);
+  });
+});
